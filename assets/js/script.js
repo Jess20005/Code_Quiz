f9c5459bdd5f920856f704e2a85a7b3fdad31b77
Jessica <simply.me05@hotmail.com>
2021-03-11 16:13:19 -0500

    set layout for script to  run through of questions, few changes to html and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,108 @@
-//WHat do expect to happen when we click on start button:
-//1-Remove game description
-//2-Display first question and answer choices
-var questionIndex = 0;
-var titleEl = document.getElementById("title");
-var startButton = document.getElementById("button");
-//What do we expect when we click on the answer choice?
-//1- Check to see if the answer is correct
-//1.1- What do we do if the answer is not correct? Then penalize the user 15 seconds
-//1.2- IF the answer is correct console log answer is correct
-//2-Display next question
-//2.1 Repeat process -check to see if answer is correct line 6
-
-function startQuiz() {
-  //1-Remove game description
-  // set a class attribute to the description
-  //2-Display first question and answer choices
-  displayQuestion();
-}
-// startButton.onclick = startQuiz;
-
-function displayQuestion() {
-  //get the right question and display to the screen
-  //1- Check to see if the answer is correct
-  //1.1- What do we do if the answer is not correct? Then penalize the user 15 seconds
-  //1.2- IF the answer is correct console log answer is correct
-  var currentQuestion = questions[questionIndex];
-  titleEl.textContent = currentQuestion.questionTitle;
-
-  // get the element for the html file
-
-}
-
 var questions = [
   {
-    questionTitle: "What is 2*3?",
-    questionChoices: ["4", "5", "6", "7"],
-    correctAnswer: "6",
+    question: "Commonly used data types DO NOT include:",
+    answerChoices:["strings","booleans","alerts","numbers"],
+    correctAnswer:"alerts",
   },
   {
-    questionTitle: "What is 2*3?",
-    questionChoices: ["4", "5", "6", "7"],
-    correctAnswer: "6",
+    question: "The condition in an if/else statement is enclosed within___________.",
+    answerChoices:["quotes","parenthesis","curly brackets","square brackets"],
+    correctAnswer:"parenthesis",
   },
   {
-    questionTitle: "What is 2*3?",
-    questionChoices: ["4", "5", "6", "7"],
-    correctAnswer: "6",
+    question: "Arrays in JavaScript can be used to store________.",
+    answerChoices:["numbers and strings","other arrays","booleans","all the above"],
+    correctAnswer: "all the above",
   },
   {
-    questionTitle: "What is 2*3?",
-    questionChoices: ["4", "5", "6", "7"],
-    correctAnswer: "6",
+    question:"String values must be enclosed within ______________ when being assigned to variables.",
+    answerChoices:["quotes","commas","curly brackets","parenthesis"],
+    correctAnswer: "quotes",
   },
   {
-    questionTitle: "What is 2*3?",
-    questionChoices: ["4", "5", "6", "7"],
-    correctAnswer: "6",
+    question:"A very useful tool used during development and debugging for printing content to debugger is:",
+    answerChoices:["terminal/bash","JavaScript","console log","for loops"],
+    correctAnswer: "console log",
   },
 ];
 
+var startButton = document.getElementById ("start");
+var quiz = document.getElementById("quiz");
+var timer = document.getElementById("timer");
+var question = document.getElementById ("question");
+var highscore = document.getElementById("highscore");
+var answerChoices= document.getElementById("answerChoices");
+var questionIndex = 0;
 
 
+
+//WHat do expect to happen when we click on start button:
+//1-Remove game description
+//2-Display first question and answer choices
+
+
+
+var timer= 10;
+var timeLeft=0;
+
+startButton.onclick = startQuiz;
+
+function startQuiz() {
+  // console.log("This is coming from start quiz")
+  // if (timeLeft<=timer){
+    //   timeLeft.innerHTML=timeLeft;
+    //   timeLeft++;
+    // } else{
+      //   timeLeft=0;
+      //   // wrongAnswer();
+      //   if (questionIndex<lastQuestionIndex){
+        //     questionIndex++;
+        //     displayQuestion();
+        //   } else {clearInterval(timer);
+        //     getScore();
+        //   }
+        // }
+        displayQuestion()
+      }
+      // timer= setInterval(startQuiz,1000);
+      
+      
+      
+      function displayQuestion() {
+        //get the right question and display to the screen
+        var currentQuestion = questions[questionIndex]
+        // console.log(currentQuestion)
+        
+        var titleElement = document.getElementById("questionTitle")
+        titleElement.textContent= currentQuestion.question
+        
+        answerChoices.textContent = "";
+        
+        for (var i=0;i<currentQuestion.answerChoices.length; i++){
+          var choiceElement = document.createElement("button")
+          choiceElement.onclick=handleChoiceClick
+          choiceElement.textContent = currentQuestion.answerChoices[i]
+          choiceElement.setAttribute("value",currentQuestion.answerChoices[i])
+          answerChoices.appendChild(choiceElement)
+        }
+      }
+      
+
+      function handleChoiceClick() {
+        // check if the click answer is corret
+        // go to the next question - done
+        if (this.value === questions[questionIndex].correctAnswer) {
+          confirm("Correct!")
+          console.log("Correct!")
+        } else confirm("Wrong!") 
+        console.log("Wrong!")
+        
+        questionIndex++;
+        displayQuestion()
+      }
+      
+      
+            
+  
+      
+      
